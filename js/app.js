bc13710d398e6862c662af0d940e481540c9a40d
Pedro Augusto O. Reis <pdroreis@gmail.com>
2018-11-27 18:12:11 -0200

    inicio aula 14 separacao de accordions e serialgenerator em modulos

--- js/app.js
+++ js/app.js
@@ -1,1 +1,3 @@
-angular.module("listaTelefonica", ["ngMessages"]); // colchetes servem para importar outros modulos
+// por possui o array, vc está declarando um modulo.
+// o uso sem o array, vc está buscando um modulo
+angular.module("listaTelefonica", ["ngMessages","serialGenerator", "uiAccordion"]); // colchetes servem para importar outros modulos
