fce14425cd4b8b3f4a7824e08b8ccfd9aa901b57
Pedro Augusto O. Reis <pdroreis@gmail.com>
2018-11-28 14:45:06 -0200

    separacao contatos html funcionando mensagem erro check status api

--- js/app.js
+++ js/app.js
@@ -1,3 +1,3 @@
 // por possui o array, vc está declarando um modulo.
 // o uso sem o array, vc está buscando um modulo
-angular.module("listaTelefonica", ["ngMessages","serialGenerator", "uiAccordion"]); // colchetes servem para importar outros modulos
+angular.module("listaTelefonica", ["ngMessages","serialGenerator", "uiAccordion","ngRoute"]); // colchetes servem para importar outros modulos
